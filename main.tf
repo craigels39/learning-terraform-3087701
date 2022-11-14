4c49baf7834c301731d0724c092e99b762335cc0
craigels39 <craigels39@gmail.com>
2022-11-14 15:25:12 -0700

    Update main.tf
    typo

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow HTTP/S IN - Allow All OUT"
-  vpc_id      = data.aws_vpn.default.id
+  vpc_id      = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
